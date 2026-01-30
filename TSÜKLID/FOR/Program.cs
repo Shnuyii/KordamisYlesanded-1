febbcc7b554e568464befaf0f24c444c4c382ea8
Opilane TTHK <opilane@tthk.ee>
2026-01-30 11:38:02 +0200

    Ülesanne FOR

--- TSÜKLID/FOR/Program.cs
+++ TSÜKLID/FOR/Program.cs
@@ -14,6 +14,24 @@
             // tsükli tegevusena kuva kasutajale välja i, aga liida sellele üks juurde et lugemine algaks arvust 1
             // peale tsüklit kuva kasutajale sõnum "tsükkel lõppes"
 
+            int kuiPaljuOn = 0;
+            Console.WriteLine("Tere, mitu numbrit soovite?");
+            kuiPaljuOn = int.Parse(Console.ReadLine());
+            if (kuiPaljuOn > 0)
+            {
+                for (int i = 0; i < kuiPaljuOn; i++)
+                {
+                    Console.WriteLine(i+1);
+                }
+                Console.WriteLine("Tsükkel lõppes.");
+            }
+            else
+            {
+                Console.WriteLine("Palun sisestada number suurem kui 0");
+            }
+            
+
+
             // 2. "Ruut"
             // tee muutuja "ruudukülg" milles on täisarv 0
             // kuva kasutajale tekst millega küsid kasutajalt kui suurt ruutu ta tahab
